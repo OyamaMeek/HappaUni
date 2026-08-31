a99f9b58f8d3ec90d6789794d8d64c6804b51a47
oyamahappa <oyamameek@gmail.com>
2026-08-31 09:33:28 +0800

    first commit

---
+++ HappaUni/HappaUni/HappaUniApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappaUniApp.swift
+//  HappaUni
+//
+//  Created by OyamaHappa on 2026/8/31.
+//
+
+import SwiftUI
+
+@main
+struct HappaUniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HappaUni/HappaUniTests/HappaUniTests.swift
@@ -0,0 +1,16 @@
+//
+//  HappaUniTests.swift
+//  HappaUniTests
+//
+//  Created by OyamaHappa on 2026/8/31.
+//
+
+import Testing
+
+struct HappaUniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
